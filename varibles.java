e70ed40dbe03c37d93063a12535434cf652f9494
Garv Pankaj <garvpankaj22@gmail.com>
2025-02-22 13:19:29 +0530

    var

---
+++ varibles.java
@@ -0,0 +1,15 @@
+public class varibles {
+    public static void main(String args[]){
+        int a = 30;
+        int b = 69;
+        int sum = a + b;
+        System.out.println("The sum of " +a+ " and " +b+ " is ="+sum);
+        int diff = a - b;
+        System.out.println("The difference of " +a+ " and "+b+ " is ="+diff);
+        int mul = a * b;
+        System.out.println("The product of " +a+ " and " +b+ " is ="+mul);
+        int div = a / b;
+        System.out.println("The division of " +a+ " and "+b+ " is ="+div);
+    }
+    
+}
