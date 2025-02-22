fc614601e6d0267560e130ade9594b4cac118d61
Garv Pankaj <garvpankaj22@gmail.com>
2025-02-22 12:55:39 +0530

    new

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Namaste Java");
+        System.out.println("Aur mai kya kaise ho uncle ji !");
+        System.err.println("Naya error aaya bhai" );
     }
 }
