b7984bba70d99f6df4bd51c45277076cc9852a01
Garv Pankaj <garvpankaj22@gmail.com>
2025-02-24 20:59:21 +0530

    day 3

---
+++ loop.java
@@ -0,0 +1,8 @@
+public class loop {
+    public static void main(String[] args) {
+        for(int i= 0 ; i <= 10 ; i++){
+           System.out.println("No from 1 to 10 is :"+i );
+        }
+    }
+    
+}
