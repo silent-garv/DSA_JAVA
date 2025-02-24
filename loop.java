833db6a95835b06967a8abbc74296e002fd367e9
Garv Pankaj <garvpankaj22@gmail.com>
2025-02-24 21:06:24 +0530

    while

--- loop.java
+++ loop.java
@@ -1,8 +1,13 @@
 public class loop {
     public static void main(String[] args) {
-        for(int i= 0 ; i <= 10 ; i++){
-           System.out.println("No from 1 to 10 is :"+i );
-        }
-    }
+        //for(int i= 0 ; i <= 10 ; i++){
+         //  System.out.println("No from 1 to 10 is :"+i );
+        //}
     
+        int i = 0 ;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        } 
+}
 }
