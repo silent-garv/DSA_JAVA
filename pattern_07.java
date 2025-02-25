395ce3be2a208bbdebfcfc8efe5f40d30f771723
Garv Pankaj <garvpankaj22@gmail.com>
2025-02-25 23:58:19 +0530

    day 04

---
+++ pattern_07.java
@@ -0,0 +1,11 @@
+public class pattern_07 {
+    public static void main(String[] args) {
+        int n = 9;
+        for(int i = n ; i >= 1 ; i--){
+            for(int j = 1; j<=i ; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
