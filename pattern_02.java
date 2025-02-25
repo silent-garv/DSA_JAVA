33e283d6b7e6dfca6edf5e26ca11c2c8b4b22d88
Garv Pankaj <garvpankaj22@gmail.com>
2025-02-25 22:08:01 +0530

    dd

--- pattern_02.java
+++ pattern_02.java
@@ -1,12 +1,10 @@
-public class pattern_02 {
+class pattern_02{
     public static void main(String[] args) {
-        int n = 4;
+        int n =4;
         int m = 5;
-        for(int i = 1; i <= n ; i++) {
-            
-            for(int j = 1; j <= m ; j++) { 
-
-                if(i==1 || j==1 || i==n || j==m) {
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m ;j++){
+                if(i==1||j==1||i==n||j==m){
                     System.out.print("*");
                 }
                 else{
@@ -18,4 +16,3 @@ public class pattern_02 {
        
     }
 }
-
