5a8ef1f11754bcdc129abf0adbe6a20815a5252a
Garv Pankaj <garvpankaj22@gmail.com>
2025-02-27 01:12:16 +0530

    d5

--- exe_05.java
+++ exe_05.java
@@ -1,19 +1,15 @@
 import java.util.*;
 public class exe_05 {
-    public static int   eligibility(int age){
+    public static void eligibility (int age){
         if(age>= 18){
-            return 1;
+            System.out.println("eligible");
         }else{
-            return 0;
+            System.out.println("not eligible");
         }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age  = sc.nextInt();
-        if(eligibility(age)==1){
-            System.out.println("You are eligible to vote");
-        }else{
-            System.out.println("You are not eligible to vote");
-        }
+        eligibility(age);
     }
 }
