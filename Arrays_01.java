bc9564e28eb4660bc539031ad4558652b8766b5a
Garv Pankaj <garvpankaj22@gmail.com>
2025-03-01 11:00:24 +0530

    d7

--- Arrays_01.java
+++ Arrays_01.java
@@ -4,8 +4,11 @@ public class Arrays_01 {
         marks[0] = 94;
         marks[1] = 87;
         marks[2] = 91;
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        for(int i = 0  ; i<= 3 ; i++){
+            System.out.println(marks[i]);
+        }
     }
 }
