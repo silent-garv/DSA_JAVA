bbbf0675e9b6880a5529adde68bfe2d0c8d23ded
Garv Pankaj <garvpankaj22@gmail.com>
2025-03-03 22:18:05 +0530

    d9

---
+++ Strings_06.java
@@ -0,0 +1,8 @@
+public class Strings_06 {
+    public static void main(String[] args) {
+        String sentence = "My name is Garv pankaj";
+        String name = sentence.substring(11,sentence.length());
+        System.out.println(name);
+    }
+    
+}
