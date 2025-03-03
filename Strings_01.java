16e4350ff546c595682ae92d085b13052c7556d9
Garv Pankaj <garvpankaj22@gmail.com>
2025-03-03 21:27:29 +0530

    d9

---
+++ Strings_01.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Strings_01 {
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    System.out.println("your name is :"+name);
+    sc.close();
+    }
+    
+}
+
+
