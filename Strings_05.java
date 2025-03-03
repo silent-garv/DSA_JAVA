677057adb96bccecd8c905e32e5a2ffeabe17eb7
Garv Pankaj <garvpankaj22@gmail.com>
2025-03-03 22:13:24 +0530

    d9

---
+++ Strings_05.java
@@ -0,0 +1,12 @@
+public class Strings_05 {
+    public static void main(String[] args) {
+        String name_1 = "Garv";
+        String name_2 ="Garv";
+
+        if(name_1.compareTo(name_2)== 0){
+           System.out.println("Both names are same");
+        }else{
+            System.out.println("Both names are different");
+        }
+    }
+}
