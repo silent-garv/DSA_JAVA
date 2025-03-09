7b99cb29c49ea3cab51610e24dfcebbc42ccf747
Garv Pankaj <garvpankaj22@gmail.com>
2025-03-09 22:44:26 +0530

    d14

---
+++ Sorting_bubble_sort.java
@@ -0,0 +1,23 @@
+public class Sorting_bubble_sort {
+    public static void printArray(int arr[]){
+        for(int i = 0 ; i< arr.length ; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {7, 8, 3, 1, 2};
+        // time complexity of bubble sort is O(n^2)
+        //bubble sort algorithm
+        for(int i = 0 ; i< arr.length- 1 ; i++){
+            for(int j = 0 ; j< arr.length - i - 1 ; j++){
+                if(arr[j] > arr[j+1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp ;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
