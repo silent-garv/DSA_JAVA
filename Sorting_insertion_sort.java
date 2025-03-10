cf673a20e9f10c5661e95a339e4379614bf99f5d
Garv Pankaj <garvpankaj22@gmail.com>
2025-03-10 21:16:11 +0530

    d15

--- Sorting_insertion_sort.java
+++ Sorting_insertion_sort.java
@@ -4,6 +4,8 @@ public class Sorting_insertion_sort{
             System.out.print(arr[i]+" ");
         }
     }
+    // time complexity of insertion sort is O(n^2)
+    // insertion sort algorithm
     public static void main(String[] args) {
         int arr[] = {7,8,9,3,1,2};
         for(int i = 1 ; i<arr.length ; i++){
