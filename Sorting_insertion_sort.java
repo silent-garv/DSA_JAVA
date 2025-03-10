0fa4d65c2a30eb3f078205caf85625794f77374f
Garv Pankaj <garvpankaj22@gmail.com>
2025-03-10 21:13:20 +0530

    d15

--- Sorting_insertion_sort.java
+++ Sorting_insertion_sort.java
@@ -1,21 +1,19 @@
-public class Sorting_insertion_sort {
-    public static void  printArray(int arr[]){
+public class Sorting_insertion_sort{
+    public static void printArray (int arr[]) {
         for(int i = 0 ; i< arr.length ; i++){
             System.out.print(arr[i]+" ");
         }
     }
     public static void main(String[] args) {
-        int arr[] = {7, 8, 3, 1, 2};
-        //insertion sort algorithm 
-        for(int i = 1 ; i< arr.length ; i++){
+        int arr[] = {7,8,9,3,1,2};
+        for(int i = 1 ; i<arr.length ; i++){
             int current = arr[i];
             int j = i-1;
-            while(j >= 0 && current  < arr[j]){
+            while (j >=  0  && current < arr[j]){
                 arr[j+1] = arr[j];
                 j--;
             }
-            //placement
-            arr[j+1] = current;
+            arr[j+1] = current ;
         }
         printArray(arr);
     }
