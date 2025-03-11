41aabf90933169fa0f9e64144564fd31ba4bee3b
Garv Pankaj <garvpankaj22@gmail.com>
2025-03-11 22:33:11 +0530

    d16

---
+++ p_02.java
@@ -0,0 +1,13 @@
+
+
+public class p_02 {
+    public static void main(String[] args) {
+        float sub1 = 45;
+        float sub2 = 90;
+        float sub3 = 89;
+
+        float cgpa = (sub1 + sub2 + sub3)/30;
+        System.out.println("The CGPA of the subjects is :"+cgpa);
+    }
+    
+}
