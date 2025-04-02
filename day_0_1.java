7b03fd129c264f2f7d1e54ae9d200de6dc1b91ee
Garv Pankaj <garvpankaj22@gmail.com>
2025-04-02 22:05:19 +0530

    d0

---
+++ day_0_1.java
@@ -0,0 +1,15 @@
+public class day_0_1 {
+    public static void main(String[] args) {
+        int a = 6 * 5 - 34 / 2 ;
+        // 30 - 34/2
+        // 30 - 17
+        // 13
+
+        int b = 60/5-34*2;
+        // 12 - 34*2
+        // 12 - 68
+        // -56
+        
+        System.out.println(a);
+}
+}
