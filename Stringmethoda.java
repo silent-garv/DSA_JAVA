15f09731da214c229f680a7952fa820cd0954f75
Garv Pankaj <garvpankaj22@gmail.com>
2025-04-04 23:04:29 +0530

    d0

--- Stringmethoda.java
+++ Stringmethoda.java
@@ -1,7 +1,26 @@
+
+
 public class Stringmethoda {
     public static void main(String[] args) {
         String name = "Garv";
+        int vlue = name.length();
         System.out.println(name);
+        System.out.println(vlue);
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+
+        String nonTrimedString = "                       Garv                          ";
+        System.out.println(nonTrimedString);
+        System.out.println(nonTrimedString.trim());
+        System.out.println(name.substring(1));
+        System.out.println(name.substring(1,3));
+        System.out.println(name.replace('G','C'));
+        System.out.println(name.startsWith("Ga"));
+        System.out.println(name.endsWith("rv"));
+        System.out.println(name.charAt(2));
+        System.out.println(name.indexOf("r"));
+
+
 
 
     }
