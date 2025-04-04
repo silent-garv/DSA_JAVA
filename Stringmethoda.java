ad85de1952d229c822611b76401eb2a3fa06516a
Garv Pankaj <garvpankaj22@gmail.com>
2025-04-04 22:16:13 +0530

    d1

---
+++ Stringmethoda.java
@@ -0,0 +1,8 @@
+public class Stringmethoda {
+    public static void main(String[] args) {
+        String name = "Garv";
+        System.out.println(name);
+
+
+    }
+}
---
+++ s1.java
@@ -0,0 +1,9 @@
+public class s1 {
+    public static void main(String[] args) {
+        String name = "Garv" ;
+        //name = new String("Garv");
+        //System.out.println(name);
+        System.out.format(name);
+
+    }
+}
