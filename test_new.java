04030540d2e297c4344db69530b865adb8407d10
Garv Pankaj <garvpankaj22@gmail.com>
2025-06-05 22:41:07 +0530

    j

--- test_new.java
+++ test_new.java
@@ -1,5 +1,14 @@
 public class test_new{
 public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    int Num = 6;
+    char c1 ='*';
+
+    for (int i = 0 ; i<=4 ; i++){
+        for(int j = 1; j<=i; j++){
+            System.out.print(Num);
+        }
+        System.out.println();
+    }
+
 }
 }
